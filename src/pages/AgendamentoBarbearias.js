2e0f5e0a41e3b705fc79a8e3a3777ee69d81515b
JulliaThainna <julliathainna@outlook.com>
2023-10-31 00:14:51 -0300

    Fazendo modal

--- src/pages/AgendamentoBarbearias.js
+++ src/pages/AgendamentoBarbearias.js
@@ -1,22 +1,35 @@
-import { Text, View} from "react-native";
-import Styles from "../components/styles/Styles";
-import LogoSecundaria from "../components/LogoSecundaria";
-import BarraDePesquisa from "../components/BarraDePesquisa";
-import BotaoCadastrar from "../components/BotaoCadastrar";
-import BotaoDeletar from "../components/BotaoDeletar";
-import Tabela from "../components/TabelaAgendamentosBarbearia";
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import Styles from '../components/styles/Styles';
+import LogoSecundaria from '../components/LogoSecundaria';
+import BarraDePesquisa from '../components/BarraDePesquisa';
+import BotaoCadastrar from '../components/BotaoCadastrar';
+import BotaoDeletar from '../components/BotaoDeletar';
+import Tabela from '../components/TabelaAgendamentosBarbearia';
+import Teste from '../components/Teste';
 
 export default function AgendamentoBarbearia(props) {
-    return(
-        <View style={Styles.appDefault}>
-            <LogoSecundaria>Agendamentos</LogoSecundaria> 
-            <BarraDePesquisa></BarraDePesquisa>
-            <View style={{flexDirection: 'row', marginTop: 40, marginBottom: 30}}>
-                <BotaoCadastrar>Agendar</BotaoCadastrar>
-                <BotaoDeletar>Deletar todos</BotaoDeletar>
-            </View>
-            <Text style={[Styles.textLogoSecundaria, {fontSize: 25, marginBottom: 10, alignSelf: 'flex-start', marginLeft: 30}]}>Dia 05/10/2023</Text>  
-            <Tabela></Tabela>
-        </View>
-    )
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleAgendarPress = () => {
+    setModalVisible(true);
+  };
+
+  const handleModalClose = () => {
+    setModalVisible(false);
+  };
+
+  return (
+    <View style={Styles.appDefault}>
+      <LogoSecundaria>Agendamentos</LogoSecundaria>
+      <BarraDePesquisa />
+      <View style={{ flexDirection: 'row', marginTop: 40, marginBottom: 30 }}>
+        <BotaoCadastrar onPress={handleAgendarPress} />
+        <BotaoDeletar>Deletar todos</BotaoDeletar>
+      </View>
+      <Text style={[Styles.textLogoSecundaria, { fontSize: 25, marginBottom: 10, alignSelf: 'flex-start', marginLeft: 30 }]}>Dia 05/10/2023</Text>
+      <Teste visible={modalVisible} onClose={handleModalClose} />
+      <Tabela />
+    </View>
+  );
 }
