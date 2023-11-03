5bb0da6ebd480a30bff75e12ee4572c548ddc612
JulliaThainna <julliathainna@outlook.com>
2023-11-03 03:39:40 -0300

    arrumando scrollview, design e organizacao de arquivos

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import { registerRootComponent } from 'expo';
 
 import GetStarted from './src/pages/GetStarted';
-import LoginC from './src/pages/Clientes/Login';
-import LoginB from './src/pages/Barbearia/Login';
-import HomeC from './src/pages/Clientes/Home';
-import HomeB from './src/pages/Barbearia/Home';
-import AgendamentosC from './src/pages/Clientes/Agendamentos';
-import AgendamentosB from './src/pages/Barbearia/Agendamentos';
+import LoginC from './src/pages/Clientes/LoginC';
+import LoginB from './src/pages/Barbearia/LoginB';
+import HomeC from './src/pages/Clientes/HomeC';
+import HomeB from './src/pages/Barbearia/HomeB';
+import AgendamentosC from './src/pages/Clientes/AgendamentosC';
+import AgendamentosB from './src/pages/Barbearia/AgendamentosB';
 import Perfil from './src/pages/Barbearia/Perfil';
-import ServicosC from './src/pages/Clientes/Servicos';
-import ServicosB from './src/pages/Barbearia/Servicos';
+import ServicosC from './src/pages/Clientes/ServicosC';
+import ServicosB from './src/pages/Barbearia/ServicosB';
 import Clientes from './src/pages/Barbearia/Clientes';
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
